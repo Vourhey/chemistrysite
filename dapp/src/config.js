33017c4522652e758ea0dabba9cb723d0dab423b
Vadim Manaenko <vadim.razorq@gmail.com>
2018-09-11 19:52:31 +0300

    Add dapp source

---
+++ dapp/src/config.js
@@ -0,0 +1,3 @@
+export const LIGHTHOUSE = 'chemistry.lighthouse.0.robonomics.eth'
+export const MODEL = 'QmWboFP8XeBtFMbCeMiStRY3gKFBSR5iQzkKgeNgQz3dZ5'
+export const OBJECTIVE = 'QmUo3vvSXZPQaQWjb3cH3qQo1hc8vAUqNnqbdVABbSLb6r'
