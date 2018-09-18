4cb978440ae347416bf822ad3f7a6451c0e1a9d8
Vadim Manaenko <vadim.razorq@gmail.com>
2018-09-18 12:16:37 +0300

    added dapp template

---
+++ dapp-template/template/src/config.js
@@ -0,0 +1,7 @@
+export const NETWORK = 1
+export const LIGHTHOUSE = 'airalab.lighthouse.1.robonomics.eth'
+export const MODEL = 'QmWboFP8XeBtFMbCeMiStRY3gKFBSR5iQzkKgeNgQz3dZ5'
+export const OBJECTIVE = 'QmUo3vvSXZPQaQWjb3cH3qQo1hc8vAUqNnqbdVABbSLb6r'
+export const IPFS_PUBSUB = 'https://wss.pool.aira.life'
+export const ENS = ''
+export const VERSION = 0
